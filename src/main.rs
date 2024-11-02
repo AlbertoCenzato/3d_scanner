dde2f7901a0d1bc4fe0f20998c04ec1838698527
Alberto Cenzato <alberto.cenzato@outlook.it>
2024-11-02 18:47:10 +0100

    websocket server

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,13 @@ mod logging;
 mod motor;
 
 use calibration::load_calibration;
-use motor::make_stepper_motor;
-
 use logging::make_logger;
+use motor::make_stepper_motor;
 
 use anyhow::Result;
 use clap::{Parser, Subcommand};
 use std::path::PathBuf;
+use websocket::sync::Server;
 
 #[derive(Parser)]
 struct Cli {
@@ -33,6 +33,9 @@ enum Commands {
     },
     Motor {
         degrees: f32,
+    },
+    Server {
+        port: u16,
     },
 }
 
@@ -72,6 +75,52 @@ fn main() -> Result<()> {
             let steps = (degrees / 360_f32 * steps_per_rev) as u32;
             println!("Moving motor {} degrees, {} steps", degrees, steps);
             motor.step(steps);
+        }
+        Commands::Server { port } => {
+            let connection_string = format!("0.0.0.0:{}", port);
+            let server = Server::bind(connection_string)?;
+            println!("WebSocket server listening on port {}", port);
+
+            for request in server.filter_map(Result::ok) {
+                if let Ok(client) = request.accept() {
+                    let peer_address = client.peer_addr()?;
+                    println!("Connection from {}", peer_address);
+
+                    let (mut receiver, mut sender) = client.split()?;
+                    for message in receiver.incoming_messages() {
+                        if let Err(e) = message {
+                            println!("Error: {:?}", e);
+                            break;
+                        }
+
+                        let message = message.unwrap();
+                        println!("Received message: {:?}", message);
+                        match message {
+                            websocket::OwnedMessage::Close(_) => {
+                                let message = websocket::Message::close();
+                                sender.send_message(&message).unwrap();
+                                println!("Client {} disconnected", peer_address);
+                                break;
+                            }
+                            websocket::OwnedMessage::Ping(ping) => {
+                                let message = websocket::Message::pong(ping);
+                                sender.send_message(&message).unwrap();
+                                println!("Ponged");
+                            }
+                            websocket::OwnedMessage::Text(text) => {
+                                println!("Text message received: {}", text);
+                            }
+                            websocket::OwnedMessage::Binary(_) => {
+                                println!("Binary message received, not supported");
+                            }
+                            _ => {
+                                println!("Unsupported message type");
+                            }
+                        };
+                    }
+                    println!("Listening for incoming connections...");
+                }
+            }
         }
     }
 
