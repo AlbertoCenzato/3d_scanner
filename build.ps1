edd3d95cd6e45fe1d3c989baae3aa11229ccbf08
Alberto Cenzato <alberto.cenzato@outlook.it>
2024-12-29 23:06:51 +0100

    Create a named volume to persist sccache files

--- build.ps1
+++ build.ps1
@@ -3,9 +3,11 @@ $platform = "linux/arm/v7"
 
 docker build -t $imageName --platform $platform "$PSScriptRoot/docker"
 
+docker volume create sccache
+
 docker run -it --rm `
 	-v ${PSScriptRoot}:/workspace `
-	-v $env:SCCACHE_DIR:/root/.cache/sccache `
+	-v sccache:/root/.cache/sccache `
 	--platform $platform `
 	-w /workspace `
 	-e SCCACHE_DIR=/root/.cache/sccache `
