862f8376358700840a062d539f0e4f59f907ead5
Alberto Cenzato <alberto.cenzato@outlook.it>
2024-12-30 15:31:59 +0100

    Persist CARGO_HOME in docker volume

--- build.ps1
+++ build.ps1
@@ -3,13 +3,12 @@ $platform = "linux/arm/v7"
 
 docker build -t $imageName --platform $platform "$PSScriptRoot/docker"
 
-docker volume create sccache
+docker volume create cargo-home
 
 docker run -it --rm `
 	-v ${PSScriptRoot}:/workspace `
-	-v sccache:/root/.cache/sccache `
+	-v cargo-home:/root/.cargo `
 	--platform $platform `
 	-w /workspace `
-	-e SCCACHE_DIR=/root/.cache/sccache `
 	$imageName `
 	/bin/bash -c "cargo build --release --bin scanner_3d --target armv7-unknown-linux-gnueabihf"
