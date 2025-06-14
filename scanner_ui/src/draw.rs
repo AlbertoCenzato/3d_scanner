30b8058642e664d1879b7801a50afa828082cfb0
Alberto Cenzato <alberto.cenzato@outlook.it>
2025-06-14 19:46:50 +0200

    Bugfix: axis drawing

--- scanner_ui/src/draw.rs
+++ scanner_ui/src/draw.rs
@@ -1,4 +1,4 @@
-use glam::{Mat4, Vec3};
+use glam::Vec3;
 
 pub fn circumference(r: f32, center: Vec3, v1: Vec3, v2: Vec3, points: &mut Vec<Vec3>) {
     for t in 0..360 {
@@ -28,8 +28,7 @@ pub fn segment(p1: Vec3, p2: Vec3) -> Vec<Vec3> {
 
 pub fn cylinder(r: f32, origin: Vec3, axis: Vec3, steps: u32, points: &mut Vec<Vec3>) {
     let top_face = origin + axis;
-    let v1 = axis.any_orthonormal_vector();
-    let v2 = axis.cross(v1).normalize();
+    let (v1, v2) = axis.normalize().any_orthonormal_pair();
     circle(r, origin, v1, v2, points);
     for step in 0..steps {
         let s = (step as f32) / (steps as f32);
@@ -43,24 +42,26 @@ fn lerp(a: f32, b: f32, s: f32) -> f32 {
     a + (b - a) * s
 }
 
-pub fn cone(base_radius: f32, origin: Vec3, axis: Vec3, steps: u32, points: &mut Vec<Vec3>) {
-    let vertex_pos = origin + axis;
-    let v1 = axis.any_orthonormal_vector();
-    let v2 = axis.cross(v1).normalize();
-    circle(base_radius, origin, v1, v2, points);
+pub fn cone(base_radius: f32, base_center: Vec3, axis: Vec3, steps: u32, points: &mut Vec<Vec3>) {
+    let vertex_pos = base_center + axis;
+    let (v1, v2) = axis.normalize().any_orthonormal_pair();
+    circle(base_radius, base_center, v1, v2, points);
     for step in 0..steps {
         let s = (step as f32) / (steps as f32);
-        let center = origin.lerp(vertex_pos, s);
+        let center = base_center.lerp(vertex_pos, s);
         let radius = lerp(0_f32, base_radius, 1_f32 - s);
         circumference(radius, center, v1, v2, points);
     }
 }
 
+fn ax(v: Vec3, radius: f32, points: &mut Vec<Vec3>) {
+    cylinder(radius, Vec3::ZERO, v, 64, points);
+    cone(2.0 * radius, v, 0.2 * v, 64, points);
+}
+
 pub fn axis(points: &mut Vec<Vec3>) {
-    cylinder(0.05, Vec3::ZERO, Vec3::X, 64, points);
-    cone(0.1, Vec3::X, 0.2 * Vec3::X, 64, points);
-    cylinder(0.05, Vec3::ZERO, Vec3::Y, 64, points);
-    cone(0.1, Vec3::Y, 0.2 * Vec3::Y, 64, points);
-    cylinder(0.05, Vec3::ZERO, Vec3::Z, 64, points);
-    cone(0.1, Vec3::Z, 0.2 * Vec3::Z, 64, points);
+    const RADIUS: f32 = 0.05;
+    ax(Vec3::X, RADIUS, points);
+    ax(Vec3::Y, RADIUS, points);
+    ax(Vec3::Z, RADIUS, points);
 }
